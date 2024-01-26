5e82425ef442afebe1b66f6c7c591139c549f274
ChangMinPark2 <98qkrckdals@naver.com>
2024-01-26 17:29:27 +0900

    feat(typeScript): generics2 실습

--- 08.generics/src/main.ts
+++ 08.generics/src/main.ts
@@ -1,27 +1,43 @@
-// function getArrayLength(arr : number[] | string []) : number {
+// // function getArrayLength(arr : number[] | string []) : number {
+// //     return arr.length;
+// // }
+//
+// function getArrayLength<T>(arr: T[]) : number {
 //     return arr.length;
 // }
+//
+// const array1 = [1, 2, 3];
+// const array2 = ["1,", "a"];
+//
+// getArrayLength<number>(array1);
+// getArrayLength<string>(array2);
+//
+// interface Vehicle<T> {
+//     name: string;
+//     option: T;
+// }
+//
+// // @ts-ignore
+// const car : Vehicle<{price: number} > = {
+//     name: 'Car',
+//     option: {
+//         price: 1000
+//     }
+// }
+//
 
-function getArrayLength<T>(arr: T[]) : number {
-    return arr.length;
+const makeArr = <X, Y>(x: X, y: Y):  [X, Y] =>{
+    return [x, y];
 }
 
-const array1 = [1, 2, 3];
-const array2 = ["1,", "a"];
+const array = makeArr<number, number>(4, 5);
+const array2 = makeArr<string, string>("$", "a");
 
-getArrayLength<number>(array1);
-getArrayLength<string>(array2);
-
-interface Vehicle<T> {
-    name: string;
-    option: T;
-}
-
-// @ts-ignore
-const car : Vehicle<{price: number} > = {
-    name: 'Car',
-    option: {
-        price: 1000
+const makeFullName = <T extends {firstName: string, lastname: string}>(obj: T) => {
+    return {
+        ...obj,
+        fullName: obj.firstName + " " + obj.lastname
     }
 }
 
+makeFullName({firstName: "si", lastname: "kim", location: 'seoul'})
